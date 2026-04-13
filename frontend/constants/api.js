d6221533f01f1be8ce4980bc98ef1efbf627714d
clickalot <rstavig@comcast.net>
2026-04-13 14:48:50 -0500

    fix api

--- frontend/constants/api.js
+++ frontend/constants/api.js
@@ -4,6 +4,6 @@ export const API_BASE_URL =
 
 // Build all endpoints from the base URL
 export const API_URL = API_BASE_URL; // General API base
-export const API_MY_RECIPES_URL = `${API_BASE_URL}/recipes/mine`;
-export const API_RECIPE_URL = `${API_BASE_URL}/recipes`;
-export const API_FAVORITES_URL = `${API_BASE_URL}/favorites`;
+export const API_MY_RECIPES_URL = `${API_BASE_URL}/api/recipes/mine`;
+export const API_RECIPE_URL = `${API_BASE_URL}/api/recipes`;
+export const API_FAVORITES_URL = `${API_BASE_URL}/api/favorites`;
